eb3d551d2b5c46a9326231815145cf454c21a38f
Utkarsh Patel <ut436516@dal.ca>
2021-11-24 11:53:40 -0400

    Added Pie chart

--- p.py
+++ p.py
@@ -1,8 +1,9 @@
 from typing import Tuple
 import json
 from pathlib import Path
-
+from plotly.subplots import make_subplots
 import dash
+import math
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -23,6 +24,7 @@ from time import sleep
 from flask import Response
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.title = "Rainfall Prediction in Australia"
 app.config.suppress_callback_exceptions=True
 # server = app.server
 
@@ -90,7 +92,8 @@ index_page = dbc.Container([
                                 "Visiting a place and wanna know about the rainfall there? We can help!",
                                 className="card-text",
                             ),
-                            dbc.Button("Click to Visualize", color="warning"),
+                            dcc.Link(dbc.Button("Click to Visualize", color="warning"), href="/location",
+                                     refresh=True),
                         ]
                     ),
                 ],
@@ -141,7 +144,7 @@ index_page = dbc.Container([
         style={"position":"relative","left":"110px","margin-top": "20px"}
     ),
 ],
-style={"margin-top": 0}
+style={"margin-top": 0,"padding-top":"50px"}
 )
 
 
@@ -248,7 +251,7 @@ def get_layout_for_tab3():
                 dbc.Label("Select Chart"),
                 dcc.RadioItems(id="dropdown_chart_corr",value=1, options=[{"label": "Heat-Map", "value": "heat-map"},
                                                                          {"label": "Pair Plot", "value": "pair"}],
-                               labelStyle={'display': 'inline-block'}),
+                               labelStyle={'display': 'inline-block','padding-right':'15px'}),
                 # dcc.Dropdown(id="dropdown_chart_corr", value=1, options=[{"label": "Heat-Map", "value": "heat-map"},
                 #                                                          {"label": "Pair Plot", "value": "pair"}]),
                 dbc.Label("Select feature"),
@@ -278,6 +281,52 @@ page1 = dbc.Container([
     ], style={'font-style': 'italic', 'color':'red', 'background-color':'black'})
 ],style={"background-color":"#ffffff"})
 
+page2 = dbc.Container([
+    html.Div([
+        dbc.Row([
+            html.H3(children='Rainfall: At your Destination'),
+        ],
+            # justify="center",
+            style={"margin-bottom": "20px", "color": "#ffffff", "background-color": "#000000", "padding-left": "1%"}
+        ),
+        dbc.FormGroup([
+            dbc.Label("Select Cities"),
+            dcc.Dropdown(id="dropdown_city_location", value=1, options=fetch_cities(), multi=True),
+            dbc.Label("Select feature"),
+            dcc.Dropdown(id="dropdown_feature_location", value=2, options=fetch_numeric_columns()),
+            html.Br()
+        ]),
+        dcc.RangeSlider(
+            id='range_slider_year',
+            min=2007,
+            max=2017,
+            step=None,
+            marks={
+                2007: '2007',
+                2008: '2008',
+                2009: '2009',
+                2010: '2010',
+                2011: '2011',
+                2012: '2012',
+                2013: '2013',
+                2014: '2014',
+                2015: '2015',
+                2016: '2016',
+                2017: '2017'
+            },
+            value=[2007, 2017]
+        ),
+        dbc.Button('Show Visualization', id='button_location', color='warning', style={'margin-bottom': '1em'},
+                   block=True),
+        dbc.Row([
+            dbc.Col(dcc.Graph(id='viz_location')),
+        ]),
+        dbc.Row([
+            dbc.Col(dcc.Graph(id='viz_directions')),
+        ]),
+    ], style={"padding-top": '20px' }),
+
+], style={"background-color": "#ffffff"})
 
 # Setting Web Layout
 page = dbc.Container([
@@ -383,8 +432,8 @@ page = dbc.Container([
 def display_page(pathname):
     if pathname == '/rainfall-history':
         return page1
-    # elif pathname == '/page-2':
-    #     return page_2_layout
+    elif pathname == '/location':
+        return page2
     else:
         return index_page
     # You could also return a 404 "URL not found" page here
@@ -497,6 +546,89 @@ def update_vis4(n_clicks, chart, feature):
         return fig
 
     return {}
+
+@app.callback(
+    dash.dependencies.Output('viz_location', 'figure'),
+    [Input('button_location', 'n_clicks')],
+    [State('dropdown_city_location', 'value'),
+     State('dropdown_feature_location', 'value'),
+     State('range_slider_year', 'value'),
+     ]
+)
+def update_vis5(n_clicks, city, feature, year_range):
+    if n_clicks:
+        fig = None
+        df_location = df[df['Location'].isin(city)]
+        years = []
+        for i in range(year_range[0], year_range[1]+1):
+            print(i)
+            years.append(i)
+        df_location = df_location[df_location['year'].isin(years)]
+        df_location['year'] = df_location['year'].apply(str)
+        df_location['year'] = df_location['year'].str[:-2]
+
+        df_location = df_location.groupby(['Location', 'year'], as_index=False)[feature].mean()
+        fig = px.line(df_location, x="year", y=feature, color='Location')
+
+        return fig
+
+    return {}
+
+@app.callback(
+    dash.dependencies.Output('viz_directions', 'figure'),
+    [Input('button_location', 'n_clicks')],
+    [State('dropdown_city_location', 'value'),
+     State('dropdown_feature_location', 'value'),
+     State('range_slider_year', 'value'),
+     ]
+)
+def update_vis6(n_clicks, cities, feature, year_range):
+    if n_clicks:
+        fig = None
+        df_location = df[df['Location'].isin(cities)]
+        print(year_range)
+        years = []
+        for i in range(year_range[0], year_range[1]+1):
+            print(i)
+            years.append(i)
+        df_location = df_location[df_location['year'].isin(years)]
+        df_location['year'] = df_location['year'].apply(str)
+        df_location['year'] = df_location['year'].str[:-2]
+        cities_len = len(cities)
+        cities_iter = (cities_len//2 + 1) if (cities_len%2 > 0) else (cities_len//2)
+        specs = []
+        for i in range(0,cities_iter):
+            specs.append([{'type':'domain'}, {'type':'domain'}])
+        print("Specifications")
+        print(specs)
+        # specs=[[{'type':'domain'}, {'type':'domain'}], [{'type':'domain'}, {'type':'domain'}]]
+        # df_location = df_location.groupby(['Location', 'year'], as_index=False)[feature].mean()
+        fig = make_subplots(rows=cities_iter, cols=2, specs=specs)
+
+        row = 1
+        col = 1
+        for city in cities:
+            df_city = df_location[df_location['Location'] == city]
+            yes_count = len(df_city[df_location['RainTomorrow'] == 'Yes'].index)
+            no_count = len(df_city[df_location['RainTomorrow'] == 'No'].index)
+            fig.add_trace(go.Pie(labels=df_location['RainTomorrow'].unique(), values=[no_count,yes_count],name=city),row, col)
+            if (col % 2) == 0:
+                row = row + 1
+                col = 1
+            else:
+                col = col + 1
+        fig.update_traces(hole=.4)
+        fig.update_layout(
+            title_text="City wise rainfall percentage"
+            # annotations=[dict(text='AliceSprings', x=0.06, y=.8, font_size=10, showarrow=False),
+            #              dict(text='Melbourne', x=0.7, y=0.8, font_size=10, showarrow=False),
+            #              dict(text='Sydney', x=0.06, y=.187, font_size=10, showarrow=False),
+            #              dict(text='Canberra', x=0.7, y=.187, font_size=10, showarrow=False)]
+            )
+        return fig
+
+    return {}
+
 
 if __name__ == "__main__":
     # app = dash_task()
